aa23cd309efd905a57576b78653c99012a44b874
massimobiagioli <biagiolimassimo@gmail.com>
2025-07-27 10:05:08 +0200

    fix: test

--- tests/routers/pages/test_home.py
+++ tests/routers/pages/test_home.py
@@ -9,4 +9,4 @@ async def test_home(
 
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
-    assert "This is the home page of your MQTT monitoring system" in response.text
+    assert "This is the home page of your IoT server" in response.text
